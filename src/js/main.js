0ea110efb098865c46247d2374029c150676079f
LauM25 <laurammwork93@gmail.com>
2025-03-10 11:24:07 +0100

    peticion al servidor

--- src/js/main.js
+++ src/js/main.js
@@ -6,7 +6,7 @@ console.log('>> Ready :)');
 
 const taskList = document.querySelector(".js_taskList");
 
-const tasks = [
+/*const tasks = [
     { name: "Recoger setas en el campo", completed: true, id: 1 },
     { name: "Comprar pilas", completed: true, id: 2 },
     { name: "Poner una lavadora de blancos", completed: true, id: 3 },
@@ -15,7 +15,7 @@ const tasks = [
         completed: false,
         id: 4,
     },
-];
+];*/
 
 /*Pasos:
 cuando la tarea sea completa, la frase sale tachada.
@@ -25,10 +25,10 @@ cuando la tarea sea completa, la frase sale tachada.
 
 // La condición if no está funcionando.
 
-for (const task of tasks) {
+/*for (const task of tasks) {
     console.log(task.name);
     if (task.completed === false) {
-        remove.classList(completed);    
+        remove.classList(completed);
     } else {
         add.classList(completed);
     }
@@ -36,10 +36,30 @@ for (const task of tasks) {
                     <input type="checkbox">
                     <p>${task.name}</p>
                 </li>`;
-   
-            
-};
+
+};*/
+const GITHUB_USER = "LauM25";
+const SERVER_URL = `https://dev.adalab.es/api/todo/${GITHUB_USER}`;
+
+fetch("https://dev.adalab.es/api/todo")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        const tasks = data.results;
+        for (const task of tasks) {
+
+            taskList.innerHTML += `<li class="list">
+                    <input type="checkbox">
+                    <p>${task.name}</p>
+                </li>`;
+        }
+    })
 
 
 
-
+function handleClick(ev) {
+    ev.preventDefault();
+    task.classList.toggle("completed")
+    task.classList.toggle("incompleted")
+}
+button.addEventListener("click", handleClick);
