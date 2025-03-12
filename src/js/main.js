73e68b3aeffc8999a07c292e56da7c31a014970b
LauM25 <laurammwork93@gmail.com>
2025-03-12 11:22:21 +0100

    localstorage

--- src/js/main.js
+++ src/js/main.js
@@ -9,31 +9,39 @@ const listCheckbox = document.querySelector(".js-list-checkbox");
 const tasksLocalStorage = JSON.parse(localStorage.getItem("tasks"));
 const taskList = document.querySelector(".js-list");
 
-fetch("https://dev.adalab.es/api/todo")
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-        const tasks = data.results;
-        for (const task of tasks) {
-            /*  if (task.completed === true) {
-                  taskList.innerHTML += `<li class="list completed js-list-checkbox">
+
+
+if (tasksLocalStorage !== null) {
+    taskList.innerHTML = `<li class="list completed js-list-checkbox">
+                <input type="checkbox">
+                <p>${task.name}</p>
+            </li>`;
+} else {
+    fetch("https://dev.adalab.es/api/todo")
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            const tasks = data.results; //una vez que se tengamos las tareas guardar con set item
+            tasksLocalStorage.setItem("task", JSON.stringify(tasks));
+            for (const task of tasks) {
+                if (task.completed === true) {
+                    taskList.innerHTML += `<li class="list completed js-list-checkbox">
                       <input type="checkbox">
                       <p>${task.name}</p>
                   </li>`;
-  
-              } else if (task.completed === false) {
-                  taskList.innerHTML += `<li class="list js-list-checkbox">
+
+                } else if (task.completed === false) {
+                    taskList.innerHTML += `<li class="list js-list-checkbox">
                       <input type="checkbox">
                       <p>${task.name}</p>
                   </li>`;
-                  */
-            if (tasksLocalStorage !== null) {
-                taskList.innerHTML = `<li class="list completed js-list-checkbox">
-                    <input type="checkbox">
-                    <p>${task.name}</p>
-                </li>`;
+
+
+                }
+
             }
         });
+}
 
 
 
