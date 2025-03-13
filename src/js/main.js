23200e008e81fca527a920e97493adc6d4f81f8f
LauM25 <laurammwork93@gmail.com>
2025-03-13 11:21:14 +0100

    fetch

--- src/js/main.js
+++ src/js/main.js
@@ -8,34 +8,38 @@ const SERVER_URL = `https://dev.adalab.es/api/todo/${GITHUB_USER}`;
 const listCheckbox = document.querySelector(".js-list-checkbox");
 const tasksLocalStorage = JSON.parse(localStorage.getItem("tasks"));
 const taskList = document.querySelector(".js-list");
-
+console.log(tasksLocalStorage)
 
 
 if (tasksLocalStorage !== null) {
-    taskList.innerHTML = `<li class="list completed js-list-checkbox">
+    for (const task of tasksLocalStorage) {
+        taskList.innerHTML += `<li class="list completed js-list-checkbox">
                 <input type="checkbox">
                 <p>${task.name}</p>
             </li>`;
+
+    }
+
+
 } else {
     fetch("https://dev.adalab.es/api/todo")
         .then(response => response.json())
         .then(data => {
-            console.log(data)
+            //console.log(data)
             const tasks = data.results; //una vez que se tengamos las tareas guardar con set item
-            tasksLocalStorage.setItem("task", JSON.stringify(tasks));
+            localStorage.setItem("tasks", JSON.stringify(tasks));
             for (const task of tasks) {
                 if (task.completed === true) {
                     taskList.innerHTML += `<li class="list completed js-list-checkbox">
-                      <input type="checkbox">
-                      <p>${task.name}</p>
-                  </li>`;
+                          <input type="checkbox">
+                          <p>${task.name}</p>
+                      </li>`;
 
                 } else if (task.completed === false) {
                     taskList.innerHTML += `<li class="list js-list-checkbox">
-                      <input type="checkbox">
-                      <p>${task.name}</p>
-                  </li>`;
-
+                          <input type="checkbox">
+                          <p>${task.name}</p>
+                      </li>`;
 
                 }
 
